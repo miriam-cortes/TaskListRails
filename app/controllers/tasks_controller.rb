db0b43d0c3ba6cc1b1d29da0ab53693647f836c7
Miriam Cortes <mcortes.umiami@gmail.com>
2016-10-01 17:17:04 -0700

    mark complete button working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,15 +25,12 @@ class TasksController < ApplicationController
   end
 
   def update
-    show
-
-    @task[:thing] = params["thing"]
-    @task[:description] = params["description"]
-    @task[:status] = params["status"]
-
-    if @task[:status]
-      @task[:complete_date] = Time.now
-    end
+    @task = task
+    @task.thing = params[:thing]
+    @task.description = params[:description]
+    @task.status = params[:status]
+    @task.save
+    redirect_to :index
   end
 
   def create
@@ -42,29 +39,34 @@ class TasksController < ApplicationController
     @task.thing = params[:task][:thing]
     @task.description = params[:task][:description]
     @task.status = params[:task][:status]
-    @task.complete_date = params[:task][:complete_date]
+    if @task.status
+      @task.complete_date = Time.now
+    end
     @task.save
     redirect_to :index
   end
 
-  # def self.alltasks
-  #   [
-  #     { id:     1,
-  #       thing:  "Brush hair",
-  #       description: "grab comb and drag it through your damn hair...seriously? Is this a question you have??? #failedatlife",
-  #       status: false,
-  #       complete_date:   nil},
-  #     { id:     2,
-  #       thing:  "Go to ADA",
-  #       description: "leave house, turn right, walk 2 blocks, board either 11 or 49, get off at 4th, walk south however many blocks.",
-  #       status: false,
-  #       complete_date: nil},
-  #     { id:     3,
-  #       thing:  "Make lame developer joke",
-  #       description: "self explanatory",
-  #       status: true,
-  #       complete_date: nil}
-  #   ]
-  # end
+  def mark_complete
+    @task = task
+    # show
+    @task.status = true
+    @task.complete_date = Time.now
+    @task.save
+    redirect_to :index
+  end
+
+  def mark_incomplete
+    show
+    @task = task
+    @task.status = false
+    @task.complete_date = nil
+    @task.save
+    redirect_to :index
+  end
+
+  private
+  def post_params
+    params.require(:task).permit(:thing, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,61 +12,10 @@ Rails.application.routes.draw do
   delete 'tasks/:id/delete' => 'tasks#delete', as: 'delete'
 # can this be /delete?? yes, but it's not convention so definitely don't do it for models
 
-  get 'tasks/edit' => 'tasks#edit', as: 'edit'
-  get 'tasks/:id/update' => 'tasks#update'
+  get 'tasks/:id/edit' => 'tasks#edit', as: 'edit'
+  get 'tasks/:id/update' => 'tasks#update', as: 'update'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  post 'tasks/:id/mark_complete' => 'tasks#mark_complete', as: 'mark_complete'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  post 'tasks/:id/mark_incomplete' => 'tasks#mark_incomplete', as: 'mark_incomplete'
 end
